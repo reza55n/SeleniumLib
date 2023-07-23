c744df68a7fd907a5afa8d4368a3b5a1a8b14f00
Reza <reza55n@gmail.com>
2023-07-23 15:28:00 +0330

    Fix

--- seleniumLib.py
+++ seleniumLib.py
@@ -19,7 +19,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.options import Options
 import datetime
 import math
 
